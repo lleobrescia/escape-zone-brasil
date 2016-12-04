07f06cd36cc27a9e86cbefa971c47c4275ecd783
Leo Brescia <leonardo@leobrescia.com.br>
2016-12-04 13:55:42 -0200

    footer

--- wp-content/themes/escapezone/footer.php
+++ wp-content/themes/escapezone/footer.php
@@ -1,3 +1,97 @@
+<?php
+/**
+* 
+* pega as informações do menu contato
+* o menu contato eh configurado pelo plugin advanced custom fields
+*/
+
+$face = get_field('facebook', 'option');
+$twitter = get_field('twitter', 'option');
+$instagram = get_field('instagram', 'option');
+$google = get_field('google', 'option');
+$linkedin = get_field('linkedin', 'option');
+
+$contato = get_field('contato_info', 'option');
+$horario = get_field('horario_info', 'option');
+?>
+
+<footer class="footer container-fluid">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-4">
+        <h4>SIGA!</h4>
+
+        <ul class="social">
+          <?php if ($face): ?>
+          <li>
+            <a href="<?php echo $face; ?>" target="_blank">
+              <i class="fa fa-facebook" aria-hidden="true"></i>
+            </a>
+          </li>
+          <?php endif ?>
+          <?php if ($twitter): ?>
+          <li>
+            <a href="<?php echo $twitter; ?>" target="_blank">
+              <i class="fa fa-twitter" aria-hidden="true"></i>
+            </a>
+          </li>
+          <?php endif ?>
+          <?php if ($instagram): ?>
+          <li>
+            <a href="<?php echo $instagram; ?>" target="_blank">
+              <i class="fa fa-instagram" aria-hidden="true"></i>
+            </a>
+          </li>
+          <?php endif ?>
+          <?php if ($google): ?>
+          <li>
+            <a href="<?php echo $google; ?>" target="_blank">
+              <i class="fa fa-google-plus" aria-hidden="true"></i>
+            </a>
+          </li>
+          <?php endif ?>
+          <?php if ($linkedin): ?>
+          <li>
+            <a href="<?php echo $linkedin; ?>" target="_blank">
+              <i class="fa fa-linkedin" aria-hidden="true"></i>
+            </a>
+          </li>
+          <?php endif ?>
+        </ul>
+      </div>
+      <!--col-sm-4-->
+      <div class="col-sm-4">
+        <h4>CONTATO</h4>
+        <?php
+          if($contato)
+          echo $contato; 
+        ?>
+      </div>
+      <!--col-sm-4-->
+      <div class="col-sm-4">
+        <h4>Horário de Funcionamento</h4>
+        <?php
+          if($contato)
+          echo $contato; 
+        ?>
+      </div>
+      <!--col-sm-4-->
+    </div>
+    <!--row-->
+  </div>
+  <!--container-->
+</footer>
+
+<div class="copy container-fluid">
+  <div class="container">
+    <small>
+     	&copy; Copyright - ESCAPE BRASIL - by  <a href="http://8dpropaganda.com.br/" target="_blank" title="8D Propaganda">8D Propaganda</a>
+    </small>
+  </div>
+  <!--container-->
+</div>
+<!--copy-->
+
 <?php wp_footer(); ?>
 </body>
 
