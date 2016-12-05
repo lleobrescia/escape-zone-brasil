c67c0432804babe496888fcbf1ca4f19391b6f23
Leo Brescia <leo.brescia@pixla.com.br>
2016-12-05 17:17:35 -0200

    configuração do menu admin

--- wp-content/themes/escapezone/functions.php
+++ wp-content/themes/escapezone/functions.php
@@ -7,8 +7,7 @@
 	 */
 	add_theme_support( 'title-tag' );
 
-//Aumenta o limite de upload
-@ini_set( 'upload_max_size' , '64M' );
+  @ini_set( 'upload_max_size' , '64M' );
 @ini_set( 'post_max_size', '64M');
 @ini_set( 'max_execution_time', '300' );
 
@@ -99,7 +98,7 @@ if(function_exists('acf_add_options_page')){
       'menu_slug'      => 'contato',
       'capability'     => 'edit_posts',
       'parent_slug'    => '',
-      'position'       => false,
+      'position'       => 4.4,
       'icon_url'       => 'dashicons-media-text'
   );
   acf_add_options_page($args );
@@ -151,4 +150,75 @@ function faq() {
 
   register_post_type( 'faq', $args );
 }
+
+// Changing the Menu Order
+function custom_menu_order($menu_ord) {
+    if (!$menu_ord) return true;
+     
+    return array(
+        'index.php', // Dashboard
+        'edit.php?post_type=product', //produtos
+        'edit.php?post_type=wc_booking', //booking
+        'woocommerce', //woocommerce
+        'separator1', //  separator
+        'edit.php?post_type=faq', // FAQ
+        'contato', // contato
+        'edit.php?post_type=page', // Pages
+        'separator2', //  separator
+        'users.php', //usuarios
+        'link-manager.php', // Links
+        'themes.php', // Appearance
+        'upload.php', // Media
+        'plugins.php', // Plugins
+        'users.php', // Users
+        'tools.php', // Tools
+        'options-general.php', // Settings
+        'wpcf7', //contact
+        'separator-last', // Last separator
+        'edit.php?post_type=acf-field-group', //ACF
+        'edit.php', // Posts
+        'edit-comments.php', // Comments
+    );
+}
+add_filter('custom_menu_order', 'custom_menu_order'); // Activate custom_menu_order
+add_filter('menu_order', 'custom_menu_order');
+
+
+// Renaming Menus and Remove
+function edit_admin_menus() {
+    global $menu;
+    global $submenu;
+     
+    $menu[27][0] = 'Reservas'; // Change booking to Reservas
+    $menu[27][6] ='dashicons-calendar-alt';
+    $submenu['edit.php?post_type=wc_booking'][5][0] = 'Todas as Reservas';
+
+    $menu[26][0] = 'Jogos'; // Change produtos to jogos
+    $submenu['edit.php?post_type=product'][5][0] = 'Jogos';
+    $submenu['edit.php?post_type=product'][10][0] = 'Adicionar Jogo';
+
+
+    remove_submenu_page('edit.php?post_type=wc_booking','edit.php?post_type=bookable_resource');//remove resource from booking
+    remove_submenu_page('edit.php?post_type=product','edit-tags.php?taxonomy=product_cat&amp;post_type=product');//remove categoria
+    remove_submenu_page('edit.php?post_type=product','edit-tags.php?taxonomy=product_tag&amp;post_type=product');//remove tag
+    remove_submenu_page('edit.php?post_type=product','product_attributes');//remove atributo
+
+    // var_dump($submenu['edit.php?post_type=product']);exit;
+}
+add_action( 'admin_menu', 'edit_admin_menus' );
+
+//Set booing for default product type
+function mw_custom_product_type_change( $product_types ) {
+  $new_array = array( 'booking' => $product_types['booking'] );
+  $product_types = $new_array;
+  return $product_types;
+}
+add_filter( 'product_type_selector', 'mw_custom_product_type_change', 20 );
+
+//Marca Virtual como Default
+function cs_wc_product_type_options( $product_type_options ) {
+    $product_type_options['virtual']['default'] = 'yes';
+    return $product_type_options;
+}
+add_filter( 'product_type_options', 'cs_wc_product_type_options' );
 ?>
