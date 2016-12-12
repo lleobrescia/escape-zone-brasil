a3c65b135d37566f44270a3d273b69f3afdca5c8
Leo Brescia <leonardo@leobrescia.com.br>
2016-12-12 04:32:48 -0200

    fix header when scroll menu para mobile

--- wp-content/themes/escapezone/js/escape.js
+++ wp-content/themes/escapezone/js/escape.js
@@ -1,28 +1,17 @@
 (function () {
-  'use strict';
+  // FIX ON SCROLL
+  jQuery(document).scroll(function () {
+    if (jQuery(window).scrollTop() > 116) {
+      jQuery('#header').addClass('fix');
+    } else {
+      jQuery('#header').removeClass('fix');
+    }
+  });
+  
+  jQuery(document).ready(function () {
+    jQuery('#menu').click(function ($) {
+      jQuery('#nav').toggleClass('mobile-hidden');
+    });
+  });
 
-  angular.module('escape', [
-
-  ]);
-})();
-
-(function () {
-  'use strict';
-
-  angular
-    .module('escape')
-    .controller('MainController', MainController);
-
-  MainController.$inject = [];
-  function MainController() {
-    var self = this;
-
-    self.enableMenu = false;
-
-    Activate();
-
-    ////////////////
-
-    function Activate() { }
-  }
 })();
